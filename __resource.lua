3f4e11060f142bf5561b14b6eec9f2e0d93e1a8c
szopendev <109073610+szopendev@users.noreply.github.com>
2022-09-20 12:43:42 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,6 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script 'client.lua'
+
+
+-- [PL] Jezeli potrzebujesz skryptow lub chcesz kupic jakies na serwer zapraszam na serwer dc: https://discord.gg/tF8UayzSZ8
+-- [EN] If you need scripts or want to buy some for server please visit dc server: https://discord.gg/tF8UayzSZ8
